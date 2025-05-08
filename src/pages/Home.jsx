e7b790289ef823228846bf8e26ccb36c152c66aa
Naveen Manjappa <NManjappa@manh.com>
2025-05-08 10:52:34 +0100

    Implement bottom banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
+import Bottombanner from '../components/Bottombanner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MainBanner/>
       <Categories/>
       <BestSeller/>
+      <Bottombanner/>
     </div>
   )
 }
