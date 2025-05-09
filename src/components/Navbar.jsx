a3161b81dd87b7ff8cafa9984d4b83d65f5cb7eb
Naveen Manjappa <NManjappa@manh.com>
2025-05-09 06:50:52 +0100

    Implement AllProducts page with search filter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
-  const { user, setUser, showUserLogin, setShowUserLogin, navigate } =
-    useAppContext();
+  const {
+    user,
+    setUser,
+    showUserLogin,
+    setShowUserLogin,
+    navigate,
+    setSearchQuery,
+    searchQuery,
+  } = useAppContext();
 
   const logout = async () => {
     setUser(null);
     navigate("/");
   };
+
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      navigate("/products");
+    }
+  }, [searchQuery]);
+
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
       <NavLink to="/" onClick={() => setOpen(false)}>
@@ -26,6 +40,7 @@ const Navbar = () => {
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
+          onChange={(e) => setSearchQuery(e.target.value)}
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
@@ -33,7 +48,10 @@ const Navbar = () => {
           <img src={assets.search_icon} alt="Search" className="w-4 h-4" />
         </div>
 
-        <div onClick={() => navigate('/cart')} className="relative cursor-pointer">
+        <div
+          onClick={() => navigate("/cart")}
+          className="relative cursor-pointer"
+        >
           <img
             src={assets.nav_cart_icon}
             alt="Cart icon"
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   //Fetch all products
   const fetchProducts = async () => {
@@ -73,7 +74,9 @@ export const AppContextProvider = ({ children }) => {
     addToCart,
     updateCartItems,
     removeFromCart,
-    cartItems
+    cartItems,
+    searchQuery,
+    setSearchQuery
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
